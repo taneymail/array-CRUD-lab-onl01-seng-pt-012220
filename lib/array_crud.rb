7a1328af46fcaef54b3fc746d63ea84547279f9d
TShipley <taneymail@gmail.com>
2020-01-30 03:44:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 def add_element_to_start_of_array(array, element)
   
   alphabet = ["a","b","c", "d"]
-  .unshift("array!")
+  ["a","b","c", "d"].unshift("array!")
 end
 
 def remove_element_from_end_of_array(array)
