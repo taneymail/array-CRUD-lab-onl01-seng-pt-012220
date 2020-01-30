bb863de26f36e4b9520f1d18da9a425378705aac
TShipley <taneymail@gmail.com>
2020-01-30 02:59:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,6 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 favorite_animals = ["cat","dog","hamster","pig"]
+favorite_animals << "koala"
 end
 
 def add_element_to_start_of_array(array, element)
