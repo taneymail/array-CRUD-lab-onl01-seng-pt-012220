c9cd574e2f919ed8ecebffa33390385f494cea22
TShipley <taneymail@gmail.com>
2020-01-30 03:42:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   
   alphabet = ["a","b","c", "d"]
-  alphabet.push("e")
+  alphabet.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
