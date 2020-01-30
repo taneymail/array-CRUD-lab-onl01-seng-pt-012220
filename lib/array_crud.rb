9a6e87ecc218d5b969dfafe665a44c012079b876
TShipley <taneymail@gmail.com>
2020-01-30 03:11:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   
-  favorite_animals = ["cat","dog","hamster","pig","koala"]
+  favorite_animals = ["cat","dog","hamster","pig"]
   favorite_animals.unshift("kangaroo")
   puts favorite_animals.inspect
 end
