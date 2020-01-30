2770b753505aa130eb559a86896e1cdbb25d7834
TShipley <taneymail@gmail.com>
2020-01-30 03:57:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 def retrieve_element_from_index(array, index_number)
   
   alphabet = ["wow", "I", "am", "really", "learning", "arrays!"]
-  alphabet[0]
+  alphabet[2]
 end
 
 def retrieve_first_element_from_array(array)
